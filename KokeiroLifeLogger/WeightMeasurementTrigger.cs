5f722cb29132170f0e449a1b4ce1f73fa99b5e3d
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-05-23 22:57:20 +0900

    体組成計のデータをテーブルに突っ込むようにした

--- KokeiroLifeLogger/WeightMeasurementTrigger.cs
+++ KokeiroLifeLogger/WeightMeasurementTrigger.cs
@@ -7,11 +7,17 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json.Linq;
 using System;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Newtonsoft.Json;
 
 namespace KokeiroLifeLogger
 {
     public static class WeightMeasurementTrigger
     {
+        public static string TableName = @"weightmeasurement";
+
         [FunctionName("WeightMeasurementTrigger")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "bodymesurement")]HttpRequestMessage req, TraceWriter log)
         {
@@ -20,20 +26,61 @@ namespace KokeiroLifeLogger
             var jsonStr = await req.Content.ReadAsStringAsync();
             log.Info(jsonStr);
 
-            var json = JObject.Parse(jsonStr);
-            log.Info(json.ToString(Newtonsoft.Json.Formatting.Indented));
+            var item = WeightMesurement.Parse(jsonStr);
 
-            var weight = (float)json["WeightKg"];
-            var leanMass = (float)json["LeanMassKg"];
-            var fatMass = (float)json["FatMassKg"];
-            var fatPercent = (float)json["FatPercent"];
-            var mesuredAtStr = (string)json["MeasuredAt"];
-            var mesuredAt = DateTime.ParseExact(mesuredAtStr, "MMM dd, yyyy 'at' hh:mmtt", new System.Globalization.CultureInfo("en-US"));
+            log.Info($"weight={item.Weight}, leanMass={item.LeanMass}, fatMass={item.FatMass}, fatPercent={item.FatPercent}, mesuredAt={item.MesuredAt}");
 
+            var table = await GetCloudTableAsync();
 
-            log.Info($"weight={weight}, leanMass={leanMass}, fatMass={fatMass}, fatPercent={fatPercent}, mesuredAt={mesuredAt}");
+            var op = TableOperation.Insert(item);
 
-            return req.CreateResponse(HttpStatusCode.OK);
+            try
+            {
+                await table.ExecuteAsync(op);
+                return req.CreateResponse(HttpStatusCode.OK,  JsonConvert.SerializeObject(item));
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message + " " + e.StackTrace);
+                throw;
+            }
+        }
+
+        private static async Task<CloudTable> GetCloudTableAsync()
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("AzureWebJobsStorage"));
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(TableName);
+            await table.CreateIfNotExistsAsync();
+            return table;
+        }
+
+        class WeightMesurement : TableEntity
+        {
+            public double Weight { get; set; }
+            public double LeanMass { get; set; }
+            public double FatMass { get; set; }
+            public double FatPercent { get; set; }
+            public DateTime MesuredAt { get; set; }
+
+            public WeightMesurement() 
+                : base("WeightMesurement", DateTime.Now.Ticks.ToString())
+            {
+            }
+
+            public static WeightMesurement Parse(string jsonStr)
+            {
+                var json = JObject.Parse(jsonStr);
+
+                return new WeightMesurement()
+                {
+                    Weight = (double)json["WeightKg"],
+                    LeanMass = (double)json["LeanMassKg"],
+                    FatMass = (double)json["FatMassKg"],
+                    FatPercent = (double)json["FatPercent"],
+                    MesuredAt = DateTime.ParseExact((string)json["MeasuredAt"], "MMM dd, yyyy 'at' hh:mmtt", new System.Globalization.CultureInfo("en-US")),
+                };
+            }
         }
     }
 }
