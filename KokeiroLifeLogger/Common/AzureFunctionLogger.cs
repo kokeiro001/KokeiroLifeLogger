1ed02ae2461f70e87451072bca40cf7d08814344
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-06-13 22:04:09 +0900

    GitHubのコントリビューション数を出力するようにした

---
+++ KokeiroLifeLogger/Common/AzureFunctionLogger.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.WebJobs.Host;
+
+namespace KokeiroLifeLogger.Common
+{
+    public static class AzureFunctionLogger
+    {
+        public static TraceWriter Logger { get; set; }
+
+        public static void Error(string message)
+        {
+            Logger?.Error(message);
+        }
+
+        public static void Information(string message)
+        {
+            Logger?.Info(message);
+        }
+
+        public static void Warning(string message)
+        {
+            Logger?.Warning(message);
+        }
+    }
+}
---
+++ KokeiroLifeLogger/Utilities/GitHubContributionsReader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Parser.Html;
+using KokeiroLifeLogger.Common;
+
+namespace KokeiroLifeLogger.Utilities
+{
+    class GitHubContributionsReader
+    {
+        static readonly string HostUrl = @"https://github.com/";
+
+        static HttpClient httpClient = new HttpClient();
+
+        public async Task<string> GetContributionsAsync(DateTime date, string username)
+        {
+            var sb = new StringBuilder();
+            sb.Append("-------------------------------------");
+
+            try
+            {
+                var contoributions = await GetContributionsCoreAsync(username);
+                var contribution = contoributions.First(x => x.Date == date.Date);
+
+                sb.AppendLine($"GitHubのコントリビューション数：{contribution.Contributions}");
+            }
+            catch (Exception e)
+            {
+                AzureFunctionLogger.Logger.Exception(e);
+                sb.AppendLine($"GitHubのコントリビューション数：(取得失敗)");
+            }
+            sb.AppendLine();
+
+            return sb.ToString();
+        }
+
+        async Task<IEnumerable<ContributionItem>> GetContributionsCoreAsync(string username)
+        {
+            var html = await GetHtmlAsync(username);
+            return ParseHtml(html);
+        }
+
+        IEnumerable<ContributionItem> ParseHtml(string html)
+        {
+            var document = new HtmlParser().Parse(html);
+            var graphCanvas = document.QuerySelectorAll("div").Where(x => x.ClassList.Contains("graph-canvas")).First();
+            var dayNodes = graphCanvas.QuerySelectorAll("rect");
+
+            return dayNodes.Select(x => new ContributionItem
+            {
+                Date = DateTime.Parse(x.GetAttribute("data-date")),
+                Contributions = int.Parse(x.GetAttribute("data-count")),
+            })
+            .OrderBy(x => x.Date);
+        }
+
+        async Task<string> GetHtmlAsync(string username)
+        {
+            var url = HostUrl + username;
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+
+    class ContributionItem
+    {
+        public DateTime Date { get; set; }
+        public int Contributions { get; set; }
+    }
+}
