342ad765be9f209a66cd24d3d59d983ae290673b
kokeiro <kokeiro.ssbm.001@gmail.com>
2019-05-08 00:36:13 +0900

    fix parse format

--- KokeiroLifeLogger/Utilities/DateTimeParser.cs
+++ KokeiroLifeLogger/Utilities/DateTimeParser.cs
@@ -5,6 +5,6 @@ namespace KokeiroLifeLogger.Utilities
     public static class DateTimeParser
     {
         public static DateTime ParseWithingsDate(string str) => 
-            DateTime.ParseExact(str, "MMMM dd, yyyy 'at' hh:mmtt", new System.Globalization.CultureInfo("en-US"));
+            DateTime.ParseExact(str, "MMM d, yyyy 'at' hh:mmtt", new System.Globalization.CultureInfo("en-US"));
     }
 }
