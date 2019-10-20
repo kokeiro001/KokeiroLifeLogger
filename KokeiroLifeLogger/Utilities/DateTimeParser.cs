eac91dd5f9e5e9b16e569599be03bc45e610ed7d
kokeiro <kokeiro.ssbm.001@gmail.com>
2019-10-20 13:44:03 +0900

    フォーマットできない月の名前があったためパースフォーマットを複数用意した

--- KokeiroLifeLogger/Utilities/DateTimeParser.cs
+++ KokeiroLifeLogger/Utilities/DateTimeParser.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Globalization;
 
 namespace KokeiroLifeLogger.Utilities
 {
     public static class DateTimeParser
     {
-        public static DateTime ParseWithingsDate(string str) => 
-            DateTime.ParseExact(str, "MMM d, yyyy 'at' hh:mmtt", new System.Globalization.CultureInfo("en-US"));
+        private readonly static string[] WithingsDateFormats = new string[]
+        {
+            "MMM d, yyyy 'at' hh:mmtt",
+            "MMMM d, yyyy 'at' hh:mmtt",
+            "MMMMM d, yyyy 'at' hh:mmtt",
+            "MMMMMM d, yyyy 'at' hh:mmtt",
+            "MMMMMMM d, yyyy 'at' hh:mmtt",
+            "MMMMMMMM d, yyyy 'at' hh:mmtt",
+            "MMMMMMMMM d, yyyy 'at' hh:mmtt",
+        };
+
+        public static DateTime ParseWithingsDate(string str)
+        {
+
+            return DateTime.ParseExact(str, WithingsDateFormats, new CultureInfo("en-US"), DateTimeStyles.None);
+        }
     }
 }
